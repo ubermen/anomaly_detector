46eb8305dd66b8e412706610dacde7deee1c4477
ubermen <wkdgus3477!>
2019-01-04 16:19:16 +0900

    Add package for cloud ml engine

---
+++ estimator/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ['tensorflow>=1.12.0']
+
+setup(
+  name='trainer',
+  version='0.1',
+  install_requires=REQUIRED_PACKAGES,
+  packages=find_packages(),
+  include_package_data=True,
+  description='Generic example trainer package with dependencies.')
