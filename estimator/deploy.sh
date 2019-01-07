3825c94faad1e1730fc776f69d53fb0b331e16ec
ubermen <wkdgus3477!>
2019-01-07 12:23:24 +0900

    Add execution scripts

--- estimator/deploy.sh
+++ estimator/deploy.sh
@@ -3,6 +3,7 @@ REGION=us-central1
 JOB_NAME=$1
 OUTPUT_PATH=gs://bigus/$JOB_NAME
 MODEL_NAME=$2
+VERSION=$3
 EXPORT_PATH=$OUTPUT_PATH/export/exporter/
 MODEL_BINARIES="$(gsutil ls $EXPORT_PATH | tail -n 1)"
-gcloud ml-engine versions create v1 --model $MODEL_NAME --origin $MODEL_BINARIES --runtime-version 1.8
+gcloud ml-engine versions create $VERSION --model $MODEL_NAME --origin $MODEL_BINARIES --runtime-version 1.8
--- estimator/predict.sh
+++ estimator/predict.sh
@@ -2,7 +2,8 @@
 REGION=us-central1
 JOB_NAME=$1
 MODEL_NAME=$2
-INPUT_PATH=$3
-OUTPUT_PATH=$4
-gcloud ml-engine jobs submit prediction $JOB_NAME --model $MODEL_NAME --version v1 --input-paths $INPUT_PATH --output-path $OUTPUT_PATH --region $REGION --data-format TEXT
+VERSION=$3
+INPUT_PATH=$4
+OUTPUT_PATH=$5
+gcloud ml-engine jobs submit prediction $JOB_NAME --model $MODEL_NAME --version $VERSION --input-paths $INPUT_PATH --output-path $OUTPUT_PATH --region $REGION --data-format TEXT
 gcloud ml-engine jobs stream-logs $JOB_NAME
