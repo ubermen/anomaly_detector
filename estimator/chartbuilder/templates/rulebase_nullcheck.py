4a7ba466595f20b110d57aaab0ef4133f153ae2b
ubermen <wkdgus3477!>
2019-03-21 10:11:52 +0900

    Sync repository

--- estimator/chartbuilder/templates/rulebase_nullcheck.py
+++ estimator/chartbuilder/templates/rulebase_nullcheck.py
@@ -9,8 +9,8 @@ SELECT *, IF(threshold < score, -score, null) AS alert FROM
         if(T1.threshold is null, 0, T1.threshold) as threshold, 
         T2.score
     FROM
-        (SELECT bucket_dt, max(threshold) as threshold FROM lqad.rulebase_nullcheck_threshold WHERE gamecode = '{gamecode}' AND `column` = '{column}' GROUP BY bucket_dt) T1 JOIN
-        (SELECT bucket_dt, max(format(null_count/total_count, 9)) AS score FROM lqad.rulebase_nullcheck_count WHERE gamecode = '{gamecode}' AND `column` = '{column}' GROUP BY bucket_dt) T2
+        (SELECT bucket_dt, max(threshold) as threshold FROM rulebase_nullcheck_threshold WHERE gamecode = '{gamecode}' AND `column` = '{column}' GROUP BY bucket_dt) T1 JOIN
+        (SELECT bucket_dt, max(format(null_count/total_count, 9)) AS score FROM rulebase_nullcheck_count WHERE gamecode = '{gamecode}' AND `column` = '{column}' GROUP BY bucket_dt) T2
         ON T1.bucket_dt = DATE_SUB(T2.bucket_dt, INTERVAL 1 DAY)
 ) T3
 '''
